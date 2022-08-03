d200ae8b6ce515f74b16325081a8ade2c39f637b
Ingrid Rocha <97315541+inerocha@users.noreply.github.com>
2022-08-03 14:16:06 -0300

    Update RegistrationControl.cs

--- Controllers/RegistrationControl.cs
+++ Controllers/RegistrationControl.cs
@@ -1,9 +1,11 @@
+using FluentResults;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using UserAPI.Data.Dto;
+using UserAPI.Data.Dto; 
+using UserAPI.Services;
 
 namespace UserAPI.Controllers
 {
@@ -12,11 +14,18 @@ namespace UserAPI.Controllers
 
     public class RegistrationControl : ControllerBase
     {
+        private CreateRecord _createRecord; 
+
+        public RegistrationControl(CreateRecord createRecord)
+        {
+            _createRecord = createRecord;
+        }
 
         [HttpPost]
         public IActionResult UserRegister(CreateUserDto createDto)
         {
-            //TODO activate the service
+            Result result = _createRecord.UserRegister(createDto);
+            if (result.IsFailed) return StatusCode(500);
             return Ok();
         }
     }
