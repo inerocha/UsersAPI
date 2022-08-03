65a350b4ef0b32a96f8635ece1dbb3b6ad348260
Ingrid Rocha <97315541+inerocha@users.noreply.github.com>
2022-08-03 14:32:43 -0300

    Create UserProfile.cs

---
+++ Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserAPI.Data.Dto;
+using UserAPI.Models;
+
+namespace UserAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<CreateUserDto, User>();
+            CreateMap<User, IdentityUser<int>>();
+        } 
+    }
+}
